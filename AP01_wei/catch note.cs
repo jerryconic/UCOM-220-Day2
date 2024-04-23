66c953487d5447d34274a27b7895128eefa13dc6
wintersneeuw <k6666617@gmail.com>
2024-04-23 13:30:14 +0800

    note update

---
+++ AP01_wei/catch note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap01_wei
+{
+    internal class catch_note
+    {
+        /*
+string s;
+int a, b;
+
+try                    
+{
+    Console.Write("a=");
+    s = Console.ReadLine();
+    a = int.Parse(s);
+
+    Console.Write("b=");
+    s = Console.ReadLine();
+    b = int.Parse(s);
+
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+catch (Exception ex)     //抓取錯誤碼ex
+{
+    Console.WriteLine(ex.Message);    //只將ex的錯誤訊息show出來?
+}
+         */
+    }
+}
---
+++ AP01_wei/tryparse note.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap01_wei
+{
+    internal class tryparse_note
+    {
+        /*
+string s;
+int a, b;
+bool iscorrect;
+
+do
+{
+    Console.Write("a=");
+    s = Console.ReadLine();
+    iscorrect = int.TryParse(s, out a);            //將s轉換型別為int，並將結果指定給a
+    if (!iscorrect)// if(iscorrect == false)         轉型別失敗後顯示
+        Console.WriteLine("a必須是整數");
+} while (!iscorrect);// while(iscorrect == false)    成功後跳脫
+
+
+do
+{
+    Console.Write("b=");
+    s = Console.ReadLine();
+    iscorrect = int.TryParse(s, out b);
+    if (!iscorrect)// if(iscorrect == false)
+        Console.WriteLine("b必須是整數");
+} while (!iscorrect);// while(iscorrect == false)
+
+Console.WriteLine($"{a} + {b} = {a + b}");
+
+         */
+    }
+}
