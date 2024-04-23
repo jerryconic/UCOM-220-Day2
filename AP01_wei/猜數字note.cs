82231bee6d54b6375e525af1fc2ab6c0a24c6173
wintersneeuw <k6666617@gmail.com>
2024-04-23 11:51:43 +0800

    note更新

---
+++ AP01_wei/猜數字note.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap01_wei
+{
+    internal class 猜數字note
+    {
+        /*
+         * //猜數字遊戲
+
+Random rnd = new Random();
+int answer = rnd.Next(1, 100);
+int guess, min = 1, max = 99;
+string s;
+
+do
+{
+    Console.Write($"請猜一個{min}-{max}的數字:");
+    s = Console.ReadLine();
+    guess = int.Parse(s);
+
+    if (guess == -1)
+    {
+        Console.WriteLine($"答案是{answer}");
+        break;                                               (直接跳出迴圈)
+    }
+    if (guess < min || guess > max)
+    {
+        Console.WriteLine($"請輸入{min} - {max}的數字");
+        continue;                                            (回歸Console.Write($"請猜一個{min}-{max}的數字:"))
+    }
+
+    if (guess > answer)
+        max = guess - 1;                                     (最大值指定成guess-1)
+    else if (guess < answer)
+        min = guess + 1;                                     (最小值指定成guess+1)
+    else
+        Console.WriteLine("猜對了");
+} while (answer != guess);
+
+         */
+
+    }
+}
---
+++ AP01_wei/複利note.cs
@@ -0,0 +1,26 @@
+﻿string s;
+int principal, n;
+double rate;
+
+Console.WriteLine("本金:");
+s = Console.ReadLine();
+principal = int.Parse(s);
+
+Console.WriteLine("利息:");
+s = Console.ReadLine();
+rate = int.Parse(s);
+
+Console.WriteLine("期數");
+s = Console.ReadLine();
+n = int.Parse(s);
+
+Console.WriteLine("期數  本      金  利      息  合      計");   //中字占2bit 中間空格6bit(不含前面2格bit)
+Console.WriteLine("====  ==========  ==========  ==========");
+for (int i = 1; i <= n; i++) 
+{
+    int interest = (int)(principal * rate / 100 + 0.5);
+    Console.WriteLine("{0,4}{1,12:#,##0}{2,12:#,##0}{3,12:#,##0}", i, principal, interest, principal + interest);
+    //Console.WriteLine($"{i,4},{principal,12},{principal + interest,12}"); 也可以這樣(*若直接以n來用期數會相同，所以用i)
+    principal += interest;
+}
+Console.WriteLine("====  ==========  ==========  ==========");
