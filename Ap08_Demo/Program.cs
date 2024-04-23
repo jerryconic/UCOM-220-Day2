4d4f602134392e2ccd128d30e42f5404343af578
Jerry Huang <jerry@conic-soft.com>
2024-04-23 11:09:26 +0800

    Guess Number V2.0

---
+++ Ap08_Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿//猜數字遊戲
+
+Random rnd = new Random();
+int answer = rnd.Next(1, 100);
+int guess, min = 1, max = 99;
+string s;
+do
+{
+    Console.Write($"請猜一個{min}-{max}的數字:");
+    s = Console.ReadLine();
+    guess = int.Parse(s);
+
+    if (guess == -1)
+    {
+        Console.WriteLine($"答案是{answer}");
+        break;
+    }
+    if (guess < min || guess > max)
+    {
+        Console.WriteLine($"請輸入{min} - {max}的數字");
+        continue;
+    }
+
+    if (guess > answer)
+        max = guess - 1;
+    else if (guess < answer)
+        min = guess + 1;
+    else
+        Console.WriteLine("猜對了");
+} while (answer != guess);
