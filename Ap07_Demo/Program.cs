4b9080fe29a17a03c35f6e111054fd4c18b37da2
Jerry Huang <jerry@conic-soft.com>
2024-04-23 11:04:01 +0800

    Guess Number V1.0 Complete

--- Ap07_Demo/Program.cs
+++ Ap07_Demo/Program.cs
@@ -10,6 +10,18 @@ do
     Console.Write("請猜一個1-99的數字:");
     s = Console.ReadLine();
     guess = int.Parse(s);
+
+    if(guess==-1)
+    {
+        Console.WriteLine($"答案是{answer}");
+        break;
+    }
+    if(guess < 1 || guess > 99)
+    {
+        Console.WriteLine("請輸入1-99的數字");
+        continue;
+    }
+
     if (guess > answer)
         Console.WriteLine("低一點");
     else if (guess < answer) 
